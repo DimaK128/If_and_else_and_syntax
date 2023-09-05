94d0346a1be1363b734050fad33200b3f0bc8cb1
DimaK128 <dmytro.kolesnyk@auk.edu.ua>
2023-09-05 03:44:19 +0300

    :)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        int time = in.nextInt();
+
+            if (time >= 5 && time <= 11) {
+                System.out.println("Good morning.");
+            } else if (time >= 12 && time <= 17) {
+                System.out.println("Good day.");
+            } else if (time <= 17 && time >= 22) {
+                System.out.println("Good evening.");
+            } else {
+                System.out.println("Good night");
+            }
+        }
     }
-}
